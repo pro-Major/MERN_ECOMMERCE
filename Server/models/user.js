f08665483f150bcb123319ea5fd521408c32bb2d
Bipin <navikbipin@gmail.com>
2021-03-18 22:38:24 +0530

    user file update

--- Server/models/user.js
+++ Server/models/user.js
@@ -4,17 +4,17 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-         name : {type : 'string' , 
+         name : {type : String , 
          required: [true , 'Please Enter Your Name'],
          maxlength : [30, 'Your Name Cannot Be More Than 30 Characters'],
 
      },
-     email : {type : 'string',
+     email : {type : String,
      required: [true, 'Please Enter Your Email Address'],
      unique : true,
      validate : [validator.isEmail, 'Please Enter Valid Email Address']
      },
-     password : {type : 'string' ,
+     password : {type : String ,
     required: [true , 'Please Enter Your Password'],
     minlength : [6,'Your Password must be at least 6 characters'],
     select : false,
@@ -23,16 +23,16 @@ const userSchema = new mongoose.Schema({
     //Thoght of adding images but not did it.
     // avatar : { 
     //     publicid : {
-    //     type : 'string',
+    //     type : String,
     //     required: [true]},
 
-    //     url : { type : 'string' ,
+    //     url : { type : String ,
     //             required : [true]
             
     //     },
     // },
 
-    role : {type : 'string' ,
+    role : {type : String ,
             default : 'user'
     },
 
@@ -41,7 +41,7 @@ const userSchema = new mongoose.Schema({
 
 
     resetPasswordToken:String,
-    resetPasswordExpires:Date
+    resetPasswordExpire:Date
 
         
         
