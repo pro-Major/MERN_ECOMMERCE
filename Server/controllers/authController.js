4a9a7a911ff5a0f6d09b16db0541374280ba82d8
Bipin <navikbipin@gmail.com>
2021-03-22 23:43:58 +0530

    login a user

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -15,9 +15,27 @@ exports.registerUser = CatchAsyncERROR ( async (req, res, next)=> {
     })
     const token = user.getJwtToken();
 
-    res.status(200).json({
+    res.status(201).json({
         success: true,
         token
     });
    
+})
+
+//Login a User
+exports.loginUser = CatchAsyncERROR (async (req,res,next)=> {
+    const {email,password} = req.body;
+
+    //checking if email and password entered by user
+    if(!email || !password){
+        return next(new ErrorHandler('Please Enter email and Password',400))
+    }
+    //Finding User in Database 
+    const user = await User.findOne({email}).select('+password')
+
+    if(!user){
+        return next(new ErrorHandler('invalid Email or Password',401));
+    }
+    //Checks If a Password is correct or not 
+    
 })
