c09148ed7ed621dfe1168fe860b80c2bcc736c08
Bipin <navikbipin@gmail.com>
2021-04-14 11:36:48 +0530

    get Orders details for single and user

--- Server/controllers/orderController.js
+++ Server/controllers/orderController.js
@@ -31,3 +31,27 @@ exports.newOrder = catchAsyncErrors(async(req,res,next)=> {
         order
     })
 })
+//Get Single Order 
+exports.getSingleOrder = catchAsyncErrors(async(req,res,next)=> {
+    const order = await Order.findById(req.params.id).populate('user','name email')
+
+    if(!order){
+        return next(new ErrorHandler(`No Order Found with this ID`,404));
+    }
+    res.status(200).json({
+        success : true,
+        order
+    })
+})
+
+
+//Get Logged in User Orders
+exports.myOrders = catchAsyncErrors(async(req,res,next)=> {
+    const orders = await Order.find({user: req.User.id})
+   
+    
+    res.status(200).json({
+        success : true,
+        orders
+    })
+})
--- Server/routes/order.js
+++ Server/routes/order.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const {newOrder} = require('../controllers/orderController');
+const {newOrder, getSingleOrder, myOrders} = require('../controllers/orderController');
 
 const {isAuthenticatedUser,authorizeRoles} = require('../middlewares/auth')
+
+
 
 router.route('/new')
 .post(isAuthenticatedUser, newOrder)
 
+
+
+router.route('/:id')
+.get(isAuthenticatedUser,getSingleOrder)
+
+
+
+router.route('/me')
+.get(isAuthenticatedUser,myOrders)
 
 
 
