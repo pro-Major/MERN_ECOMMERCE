81e75c4e161a52a6a1fdf8517084db8008178cbf
Bipin <navikbipin@gmail.com>
2021-04-16 22:54:22 +0530

    Using Helmet for Title

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,14 +1,23 @@
-import Header from './Components/layout/Header'
-import Footer from './Components/layout/Footer'
-import Home from './Components/Home'
+import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import Header from './Components/layout/Header';
+import Footer from './Components/layout/Footer';
+import Home from './Components/Home';
+import React from 'react';
+import { render } from 'react-dom';
 function App() {
-  return (
-    <div className="App">
-     <Header/>
-     <Home/>
-     <Footer/>
-    </div>
-  );
-}
+    return (
+      <Router> 
+        <div className="App">
+          <div className="container container-fluid" > 
+          <Header/>
+              <Route path='/' component={Home} exact />
+       <Footer/>
+        </div>
+        </div>
+      </Router>
+        
+    )
+  }
+
 
 export default App;
