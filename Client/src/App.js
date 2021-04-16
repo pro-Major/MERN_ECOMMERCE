9dd8746541de46fa1408daaf70fdf06731759b8d
Bipin <navikbipin@gmail.com>
2021-04-16 18:54:14 +0530

    Adding Footer

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,10 @@
 import Header from './Components/layout/Header'
+import Footer from './Components/layout/Footer'
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Footer/>
     </div>
   );
 }
