9fb6d6f2cf01fcfcdac92ae09cc083acb0dabf62
Bipin Navik <navikbipin@gmail.com>
2021-04-16 19:23:32 +0530

    Home Component Added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,11 @@
 import Header from './Components/layout/Header'
 import Footer from './Components/layout/Footer'
+import Home from './Components/Home'
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Home/>
      <Footer/>
     </div>
   );
