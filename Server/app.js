6a5d8c2cfb93ae2efa9203eedad4711107bd0305
Bipin <navikbipin@gmail.com>
2021-04-27 02:11:25 +0530

    Registration feature added

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express();
+const dotenv = require('dotenv');
 const ErrorMiddleware = require('./middlewares/errormiddleware');
 const cookieParser = require('cookie-parser');
-//JSON 
+const bodyparser = require('body-parser');
+const cloudinary = require('cloudinary');
+const fileUpload = require('express-fileupload');
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyparser.urlencoded({extended:true}));
+
+//Setting Up Config files 
+dotenv.config({path : 'Server/configuration/.env'})
+
+//Seting Up Cloudinary 
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
 //Importing All Routes 
 const products = require('./routes/products');
 const auth = require('./routes/auth');
